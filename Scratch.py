56635016deeb6de2dd3e6d89b39dbe643b7f33f2
jlenz1742 <jan-lenz@hotmail.com>
2019-04-12 16:48:38 +0200

    Connections included

--- Scratch.py
+++ Scratch.py
@@ -5,8 +5,8 @@ import igraph as ig
 l_honeycomb = 1                           # Side length of a hexagon
 l_gap = 1                                 # Distance between two layers
 diameter = 1                              # Diameter of edges
-x = 5
-y = 5
+x = 3
+y = 3
 z = 3
 
 graph = ig.Graph()
@@ -41,7 +41,6 @@ graph.add_vertices(total_vertices)
 
 # Create connections between vertices
 
-# Horizontal
 
 number_of_horizontal_edges = (x*y) + x
 
@@ -59,33 +58,155 @@ next_id = current_stop_vertex - 1
 print(next_id)
 print(graph)
 
-# Left-hand-side
+if (x % 2) == 0:
 
-current_start_vertex = 0
+    print('Even')
 
-left_slanting_list_lefthandside = []
+    starting_points_bottom = list(range(0, x))
+    starting_points_bottom.append(starting_points_bottom[-1] + x)
 
-source_vertex = 0
-target_vertex = next_id
-next_existing_vertex = 0
+    for bottom_vertex in starting_points_bottom:
 
-for i in range(2*y):
+        if bottom_vertex == 0:
 
-    print('run: ', i)
+            current_start_vertex = bottom_vertex
 
-    if (i % 2) == 0:
+            source_vertex = bottom_vertex
+            target_vertex = next_id
+            next_existing_vertex = bottom_vertex
 
-        graph.add_edge(source_vertex, target_vertex)
+            for i in range(2*y):
 
-        source_vertex = target_vertex
-        next_id += 1
-        next_existing_vertex += 2*x
-        target_vertex = next_existing_vertex
+                if (i % 2) == 0:
 
-    else:
+                    graph.add_edge(source_vertex, target_vertex)
 
-        graph.add_edge(source_vertex, target_vertex)
-        source_vertex = next_existing_vertex
-        target_vertex = next_id
+                    source_vertex = target_vertex
+                    next_id += 1
+                    next_existing_vertex += 2*x
+                    target_vertex = next_existing_vertex
+
+                else:
+
+                    graph.add_edge(source_vertex, target_vertex)
+                    source_vertex = next_existing_vertex
+                    target_vertex = next_id
+
+        elif bottom_vertex == starting_points_bottom[-1]:
+
+            current_start_vertex = bottom_vertex
+
+            source_vertex = bottom_vertex
+            target_vertex = next_id
+            next_existing_vertex = bottom_vertex
+
+            for i in range(2 * y):
+
+                if (i % 2) == 0:
+
+                    graph.add_edge(source_vertex, target_vertex)
+
+                    source_vertex = target_vertex
+                    next_id += 1
+                    next_existing_vertex += 2 * x
+                    target_vertex = next_existing_vertex
+
+                else:
+
+                    graph.add_edge(source_vertex, target_vertex)
+                    source_vertex = next_existing_vertex
+                    target_vertex = next_id
+
+        else:
+
+            source_vertex = bottom_vertex
+
+            for i in range(2*y + 1):
+
+                if (i % 2) == 0:
+
+                    graph.add_edge(source_vertex, source_vertex + x - 1)
+                    source_vertex += (x-1)
+
+                else:
+
+                    graph.add_edge(source_vertex, source_vertex + x + 1)
+                    source_vertex += (x + 1)
+
+
+else:
+
+    print('Odd')
+
+    starting_points_bottom = list(range(0, x+1))
+
+    print(starting_points_bottom)
+
+    for bottom_vertex in starting_points_bottom:
+
+        if bottom_vertex == 0:
+
+            current_start_vertex = bottom_vertex
+
+            source_vertex = bottom_vertex
+            target_vertex = next_id
+            next_existing_vertex = bottom_vertex
+
+            for i in range(2*y):
+
+                if (i % 2) == 0:
+
+                    graph.add_edge(source_vertex, target_vertex)
+
+                    source_vertex = target_vertex
+                    next_id += 1
+                    next_existing_vertex += 2*x
+                    target_vertex = next_existing_vertex
+
+                else:
+
+                    graph.add_edge(source_vertex, target_vertex)
+                    source_vertex = next_existing_vertex
+                    target_vertex = next_id
+
+        elif bottom_vertex == starting_points_bottom[-1]:
+
+            current_start_vertex = bottom_vertex
+
+            source_vertex = bottom_vertex
+            target_vertex = next_id
+            next_existing_vertex = bottom_vertex
+
+            for i in range(2 * y):
+
+                if (i % 2) == 0:
+
+                    graph.add_edge(source_vertex, target_vertex)
+
+                    source_vertex = target_vertex
+                    next_id += 1
+                    next_existing_vertex += 2 * x
+                    target_vertex = next_existing_vertex
+
+                else:
+
+                    graph.add_edge(source_vertex, target_vertex)
+                    source_vertex = next_existing_vertex
+                    target_vertex = next_id
+
+        else:
+
+            print('da')
+            source_vertex = bottom_vertex
+
+            for i in range(2*y + 1):
+
+                graph.add_edge(source_vertex, source_vertex + x )
+                source_vertex += x
+
+
 
 print(graph)
+
+
+
